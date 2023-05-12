43bd27aace06304468eccad76f05539bc22fb473
Replit user <>
2023-05-12 01:08:49 +0000

    new try

--- heap.c
+++ heap.c
@@ -25,7 +25,23 @@ void* heap_top(Heap* pq){
 
 
 void heap_push(Heap* pq, void* data, int priority){
+  if (pq->size == pq->capac) {
+      pq->capac = pq->capac * 2 + 1;
+      pq->heapArray = realloc(pq->heapArray, sizeof(heapElem) * pq->capac);
+    }
 
+  int indiceUltimoElemento = pq->size;
+  pq->heapArray[indiceUltimoElemento].data = data;
+  pq->heapArray[indiceUltimoElemento].priority = priority;
+
+  pq->size++;
+
+  while(indiceUltimoElemento > 0 && pq->heapArray[indiceUltimoElemento].priority > pq->heapArray[(indiceUltimoElemento -1) / 2].priority) { 
+  heapElem tempElemHeapArray = pq->heapArray[indiceUltimoElemento];
+  pq->heapArray[indiceUltimoElemento] = pq->heapArray[(indiceUltimoElemento -1) / 2];
+  pq->heapArray[(indiceUltimoElemento -1) / 2] = tempElemHeapArray;
+
+  indiceUltimoElemento = (indiceUltimoElemento - 1) / 2;
 }
 
 
